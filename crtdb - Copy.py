119d5009b0c55e04db91608559954c21629479c6
seemakolyaa <136303739+seemakolyaa@users.noreply.github.com>
2023-07-29 16:35:45 +0530

    Add files via upload

---
+++ crtdb - Copy.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('gendb.db')
+print("Opened database successfully")
+
+conn.execute('CREATE TABLE user(name VARCHAR(20)not null, phono VARCHAR(10) not null, email VARCHAR(50) not null,username VARCHAR(20) not null,password VARCHAR(20) not null)')
+print("Table created successfully")
+conn.close()
+
